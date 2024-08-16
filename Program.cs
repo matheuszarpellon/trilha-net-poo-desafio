864f8cf525674737d95a830b3693bb05c6a5ba61
Matheus Zarpellon <mvzarpellon@gmail.com>
2024-08-16 00:28:58 -0300

    desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("17912341234", "iPhone 12 Pro", "123456789", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+Nokia nokia = new Nokia("17943214321", "Nokia G11 Plus", "987654321", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("LinkedIn");
